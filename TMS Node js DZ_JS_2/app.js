3f92fa0b8514f83d10951ac9cd1dcad4ea7e8942
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2021-11-25 08:29:14 +0100

    add first home work

---
+++ TMS Node js DZ_JS_2/app.js
@@ -0,0 +1,11 @@
+let user = {
+    name: "Evgeny",
+    surname: "Gravdin",
+    age: 23,
+    city: "Nitra",
+    isDriver: false
+};
+
+let message = `Hi my name is ${user.name} ${user.surname}, I am ${user.age} years old, I live in ${user.city}`;
+
+console.log(message);
