8ed215470703d97a3a3b3950157becf456c167d0
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2021-12-03 20:20:19 +0100

    update code

--- TMS Node_js_DZ_9_4/script.js
+++ TMS Node_js_DZ_9_4/script.js
@@ -1,18 +1,19 @@
 let i = 1;
-alert('Hi! Let\s play the game. You have 3 attempts');
+alert('Hi! Let\s play the game.');
 let user_one = prompt('Choose a color:');
+let limit = +prompt('limit of attempts:')
 
 while (true) {
     let user_two = prompt('Guess the color:');
 
-    if (user_one == user_two && i <= 3) {
+    if (user_one == user_two && i <= limit) {
         alert(`You won on the ${i} try`);
         break
-    } else if (i == 3) {
+    } else if (i == limit) {
         alert(`Your atttempt: ${i}`);
         alert('You crossed the limit of the attempts. You lose!')
         break;
-    } else if (user_two !== user_one) {
+    } else {
         alert(`Your attempts: ${i}`);
         alert('Try again');
     }
