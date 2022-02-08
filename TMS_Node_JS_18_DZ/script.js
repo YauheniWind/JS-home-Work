28161ed50b38a94d977bac9d76087ee9f859ddde
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2022-02-08 20:23:24 +0100

    fix errors

--- TMS_Node_JS_18_DZ/script.js
+++ TMS_Node_JS_18_DZ/script.js
@@ -6,34 +6,27 @@ const cleanRoom = dirtyLevel => {
             setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000)
         }
     })
-        .catch((err) => {
-            console.log(err)
-        })
 }
 
 async function cleanRoomsStepByStep(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
     try {
         const resaltFirstRoom = await cleanRoom(dirtyLevel_1)
-        if (resaltFirstRoom === undefined) {
-            console.log('Комната слишком грязная', dirtyLevel_1)
-        } else {
-            console.log('Убрана за', resaltFirstRoom)
-        }
-
-        const resaltSecondRoom = await cleanRoom(dirtyLevel_2)
-        if (resaltSecondRoom === undefined) {
-            console.log('Комната слишком грязная', dirtyLevel_2)
-        } else {
-            console.log('Комната убрана за', resaltSecondRoom)
-        }
-
-        const resaltThirdRoom = await cleanRoom(dirtyLevel_3)
-        if (resaltThirdRoom === undefined) {
-            console.log('Комната слишком грязная', dirtyLevel_3)
-        } else {
-            console.log('Комната убрана за', resaltThirdRoom)
-        }
+        console.log('Убрана за', resaltFirstRoom)
     } catch (err) {
+        console.log('Какая-то комната слишком грязная', err)
+    }
+
+    try {
+        const resaltSecondRoom = await cleanRoom(dirtyLevel_2)
+        console.log('Комната убрана за', resaltSecondRoom)
+    } catch (err) {
+        console.log('Какая-то комната слишком грязная', err)
+    }
+    try {
+        const resaltThirdRoom = await cleanRoom(dirtyLevel_3)
+        console.log('Комната убрана за', resaltThirdRoom)
+    }
+    catch (err) {
         console.log('Какая-то комната слишком грязная', err)
     }
 }
--- TMS_Node_JS_18_DZ/script1.js
+++ TMS_Node_JS_18_DZ/script1.js
@@ -6,35 +6,20 @@ const cleanRoom = dirtyLevel => {
             setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000)
         }
     })
-        .catch((err) => {
-            console.log(err)
-        })
 }
 
 async function cleanRoomsStepByStep(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
     try {
         const resultFirstRoom = await cleanRoom(dirtyLevel_1)
-        if (resultFirstRoom === undefined) {
-            throw new Error(dirtyLevel_1)
-        } else {
-            console.log('Убрана за', resultFirstRoom)
-        }
+        console.log('Убрана за', resultFirstRoom)
 
         const resultSecondRoom = await cleanRoom(dirtyLevel_2)
-        if (resultSecondRoom === undefined) {
-            throw new Error(dirtyLevel_2)
-        } else {
-            console.log('Комната убрана за', resultSecondRoom)
-        }
+        console.log('Комната убрана за', resultSecondRoom)
 
         const resultThirdRoom = await cleanRoom(dirtyLevel_3)
-        if (resultThirdRoom === undefined) {
-            throw new Error(dirtyLevel_3)
-        } else {
-            console.log('Комната убрана за', resultThirdRoom)
-        }
+        console.log('Комната убрана за', resultThirdRoom)
     } catch (err) {
-        console.log('Какая-то из комната слишком грязная', err)
+        console.log('Какая-то из комната слишком грязная' + ' ' + err)
     }
 }
 
